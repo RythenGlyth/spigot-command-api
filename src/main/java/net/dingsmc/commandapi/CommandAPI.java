25c839c6167978b006ebf585bcfe9cfc1190b634
RythenGlyth <rythenglyth@gmail.com>
2022-01-14 00:07:08 +0100

    test

--- src/main/java/net/dingsmc/commandapi/CommandAPI.java
+++ src/main/java/net/dingsmc/commandapi/CommandAPI.java
@@ -3,5 +3,10 @@ package net.dingsmc.commandapi;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommandAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("CommandAPI has been enabled!");
+    }
     
 }
